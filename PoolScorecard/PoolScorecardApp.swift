0b8dc3517c8bc10b06eb5bdc57b3a7a5bef2abe1
Robert Fasciano <rob.fasciano@yahoo.com>
2024-10-27 16:46:21 -0400

    Initial Commit

---
+++ PoolScorecard/PoolScorecardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoolScorecardApp.swift
+//  PoolScorecard
+//
+//  Created by Robert Fasciano on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PoolScorecardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
