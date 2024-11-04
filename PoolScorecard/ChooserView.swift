8ae3ec8824e3d72b9a3ca628e3eff01ed0d6c6fa
Robert Fasciano <rob.fasciano@yahoo.com>
2024-11-03 21:26:33 -0500

    Got Selector scaling correct.

--- PoolScorecard/ChooserView.swift
+++ PoolScorecard/ChooserView.swift
@@ -10,29 +10,38 @@ import SwiftUI
 
 struct ChooserView: View {
     let feltColor = Color(red: 0.153, green: 0.365, blue: 0.167).gradient
+    let baseFontSize = 400.0
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Rectangle().foregroundStyle(feltColor)
-                VStack {
-                    Text("Select Number of Players")
-                    NavigationLink("🙄🤓") {StripeSolidView(players: 2)}
-                    NavigationLink("🙈🙉🙊") {CutthroatView()}
-                    NavigationLink("🐛🦋🐝🪲") {StripeSolidView(players: 4)}
-                    NavigationLink("🤾🏼⛹🏽‍♀️🏌️🤺🏋🏽‍♀️") {StripeSolidView(players: 1)}
+        GeometryReader {geometry in
+            NavigationStack {
+                let frameWidth = geometry.size.width
+                ZStack {
+                    Rectangle().foregroundStyle(feltColor)
+                        .ignoresSafeArea()
+                    VStack {
+                        Spacer()
+                        Text("Select Number of Players")
+                            .foregroundStyle(.teal)
+                        Spacer()
+                        NavigationLink("🙄🤓") {StripeSolidView(players: 2)}
+                            .frame(maxWidth: frameWidth*(2/5))
+                        NavigationLink("🙈🙉🙊") {CutthroatView()}
+                            .frame(maxWidth: frameWidth*(3/5))
+                        NavigationLink("🐛🦋🐝🪲") {StripeSolidView(players: 4)}
+                            .frame(maxWidth: frameWidth*(4/5))
+                        NavigationLink("🤾🏼⛹🏽‍♀️🏌️🤺🏋🏽‍♀️") {StripeSolidView(players: 1)}
+                            .frame(maxWidth: frameWidth)
+                        Spacer()
+                    }
+                    .padding()
+                    
                 }
-                .padding()
             }
-            .ignoresSafeArea()
+            .font(.system(size: baseFontSize))
+            .minimumScaleFactor(0.01)
+            .lineLimit(1)
         }
-//        .navigationTitle("My List")
-//        .scrollContentBackground(.hidden)// Add this
-//        .background(feltColor)
-//        .backgroundStyle(feltColor)
-        .font(.system(size: 200))
-        .minimumScaleFactor(0.01)
-        .lineLimit(1)
     }
 }
 
