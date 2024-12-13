71a2bca773cbebb69b726864be43da2a5b2b7e63
Robert Fasciano <rob.fasciano@yahoo.com>
2024-12-13 17:32:05 -0500

    Adding some animations

--- PoolScorecard/CutthroatView.swift
+++ PoolScorecard/CutthroatView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 var landscape: Bool = true
 
 struct CutthroatView: View {
-    let feltColor = Color(red: 0.153, green: 0.365, blue: 0.167).gradient
+    
     
     @State private var names = ["", "", ""]
     @State private var results = [
@@ -23,9 +23,8 @@ struct CutthroatView: View {
     private let midBalls = [6, 7 ,8, 9, 10]
     private let hiBalls = [11, 12, 13, 14, 15]
     
-    
+        
     var body: some View {
-        let nameSize: CGFloat = 100
         ZStack{
             GeometryReader {geometry in //TODO: this landscape variable only gets updated when first entering the View
                 landscape = true
@@ -36,17 +35,17 @@ struct CutthroatView: View {
                     Spacer()
                     Grid() {
                         ballGroups
-                        nameRow(0, height: (geometry.size.height / 3) / (landscape ? 2.2 : 2.7))
-                        nameRow(1, height: (geometry.size.height / 3) / (landscape ? 2.2 : 2.7))
-                        nameRow(2, height: (geometry.size.height / 3) / (landscape ? 2.2 : 2.7))
+                        nameRow(0, height: (geometry.size.height / 3) / (landscape ? 1 / Constants.Names.screenRatio.landscape : 1 / Constants.Names.screenRatio.portrait))
+                        nameRow(1, height: (geometry.size.height / 3) / (landscape ? 1 / Constants.Names.screenRatio.landscape : 1 / Constants.Names.screenRatio.portrait))
+                        nameRow(2, height: (geometry.size.height / 3) / (landscape ? 1 / Constants.Names.screenRatio.landscape : 1 / Constants.Names.screenRatio.portrait))
                     }
                     Spacer()
                 }
-                .font(.system(size: nameSize))
+                .font(.system(size: Constants.Names.maxFont))
                 .textFieldStyle(.automatic)
                 .multilineTextAlignment(.center)
                 .padding()
-                .background(feltColor)
+                .background(Constants.feltColor)
                 .minimumScaleFactor(0.001)
             }
         }
@@ -65,28 +64,25 @@ struct CutthroatView: View {
     
     func OneBallGroup(_ balls: [Int]) -> some View {
         GeometryReader {geometry in
-            let ballPadding = 5.0
-            let iPad = 5.0 //padding between ball (HStack) when 2 in row
-            let ballWidth = geometry.size.width - 2*ballPadding
+            let ballWidth = geometry.size.width - 2*Constants.ballPadding
 //            if geometry.size.width > geometry.size.height {
                 if landscape {
                 VStack(spacing: 0) {
                     Spacer()
-                    ballRow([balls[0], balls[1]], HSpacing: iPad)
-                        .frame(width:  ballWidth + iPad)
+                    ballRow([balls[0], balls[1]], HSpacing: Constants.iPad)
+                        .frame(width:  ballWidth + Constants.iPad)
                     ballRow([balls[2]])
                         .frame(width:  ballWidth / 2 )
-                    ballRow([balls[3], balls[4]], HSpacing: iPad)
-                        .frame(width:  ballWidth + iPad)
+                    ballRow([balls[3], balls[4]], HSpacing: Constants.iPad)
+                        .frame(width:  ballWidth + Constants.iPad)
                 }
             } else {
                 VStack {
                     Spacer()
-                    ballRow([balls[0]])
-                    ballRow([balls[1]])
-                    ballRow([balls[2]])
-                    ballRow([balls[3]])
-                    ballRow([balls[4]])
+                    //need id since [Int] dos not conform to identifiable
+                    ForEach(balls, id: \.self) { ball in
+                        ballRow([ball])
+                    }
                 }
             }
         }
@@ -139,6 +135,7 @@ struct CutthroatView: View {
             if results[player][i] {
                 Text(level)
                 Text("❌").opacity(0.7)
+                    .transition(.asymmetric(insertion: .scale.animation(.bouncy), removal: .identity))
             } else {
                 if results[otherPlayers[0]][i] && results[otherPlayers[1]][i] {
                     circleALevel(level)
@@ -238,6 +235,8 @@ struct CutthroatView: View {
         Text(level)
             .overlay(Circle().stroke(lineWidth: 7).aspectRatio(1, contentMode: .fill))
             .foregroundStyle(.red)
+            .fontWeight(.heavy)
+            .transition(.asymmetric(insertion: .scale(scale: 3).animation(.bouncy), removal: .identity))
     }
     
     
@@ -254,17 +253,33 @@ struct CutthroatView: View {
                 Image(systemName: "arrow.trianglehead.counterclockwise.rotate.90")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .font(.system(size: 80))
-                    .minimumScaleFactor(0.001)
+                    .multilineTextAlignment(.center)
                 Text("New Game")
-                    .font(.system(size: 40))
-                    .minimumScaleFactor(0.001)
+                    .font(.system(size: Constants.NewGame.maxFont))
+                    .minimumScaleFactor(Constants.NewGame.minFontScale)
                     .lineLimit(2)
             }
         }
     }
     
-    
+    private struct Constants {
+        static let feltColor = Color(red: 0.153, green: 0.365, blue: 0.167).gradient
+        static let ballPadding = 5.0
+        static let iPad = 5.0 //padding between ball (HStack) when 2 in row
+        struct NewGame {
+            static let maxFont: CGFloat = 80
+            static let minFont: CGFloat = 1
+            static let minFontScale: CGFloat = minFont / maxFont
+        }
+        struct Names {
+            static let maxFont: CGFloat = 150
+            struct screenRatio {
+                static let landscape: CGFloat = 1 / 1.8
+                static let portrait: CGFloat = 1 / 2.0
+            }
+        }
+    }
+
 }
 
 
